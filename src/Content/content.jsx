f1658a9ae43953a4c58e6de7f67f29a9eba35412
unknown <once6474@naver.com>
2026-02-02 12:37:53 +0900

    card-update

---
+++ src/Content/content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './card'
+
+const Content = () => {
+  return (
+    <div className='content'>
+        <Card/>
+        <Card/>
+        <Card/>
+    </div>
+  )
+}
+
+export default Content
